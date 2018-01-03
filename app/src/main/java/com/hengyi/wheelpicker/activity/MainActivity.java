2ca571b358e72066cf816b600e62b98dd1d539c7
fanhua1994 <18383219470@163.com>
2018-01-03 14:07:57 +0800

    修改部分问题

--- app/src/main/java/com/hengyi/wheelpicker/activity/MainActivity.java
+++ app/src/main/java/com/hengyi/wheelpicker/activity/MainActivity.java
@@ -25,7 +25,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
