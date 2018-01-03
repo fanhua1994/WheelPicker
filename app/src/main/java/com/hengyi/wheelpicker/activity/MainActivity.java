20845904aeb6cb3e0f032f35e604af217b76b1ad
fanhua1994 <18383219470@163.com>
2018-01-03 11:36:35 +0800

    初始化代码提交

---
+++ app/src/main/java/com/hengyi/wheelpicker/activity/MainActivity.java
@@ -0,0 +1,34 @@
+package com.hengyi.wheelpicker.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.hengyi.wheelpicker.R;
+import com.hengyi.wheelpicker.ppw.WheelPickerPopupWindow;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        final WheelPickerPopupWindow wheelPickerPopupWindow = new WheelPickerPopupWindow(MainActivity.this);
+        wheelPickerPopupWindow.setListener(new WheelPickerPopupWindow.WheelPickerComfirmListener() {
+
+            @Override
+            public void onSelected(String Province, String City, String District, String PostCode) {
+                Toast.makeText(MainActivity.this,Province,Toast.LENGTH_LONG).show();
+            }
+        });
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                wheelPickerPopupWindow.show();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/hengyi/wheelpicker/ppw/WheelPickerPopupWindow.java
@@ -0,0 +1,161 @@
+package com.hengyi.wheelpicker.ppw;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.hengyi.wheelpicker.R;
+import com.hengyi.wheelpicker.weight.OnWheelChangedListener;
+import com.hengyi.wheelpicker.weight.WheelView;
+import com.hengyi.wheelpicker.weight.adapters.ArrayWheelAdapter;
+
+/**
+ * Created by Administrator on 2018/1/3.
+ */
+
+public class WheelPickerPopupWindow extends BasePopupWindow implements View.OnClickListener,OnWheelChangedListener {
+    private View mView;
+    private Activity activity;
+    private TextView btn_cancel,btn_confirm;
+    private WheelView mViewProvince, mViewCity,mViewDistrict;
+    private WheelPickerComfirmListener listener = null;
+
+    public WheelPickerPopupWindow(Activity activity){
+        this.activity = activity;
+        initProvinceData(activity);
+        LayoutInflater inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mView = inflater.inflate(R.layout.ppw_wheel_picker_view, null,false);
+        btn_cancel = mView.findViewById(R.id.btn_cancel);
+        btn_confirm = mView.findViewById(R.id.btn_confirm);
+        mViewProvince = mView.findViewById(R.id.id_province);
+        mViewCity = mView.findViewById(R.id.id_city);
+        mViewDistrict = mView.findViewById(R.id.id_district);
+
+        btn_confirm.setOnClickListener(this);
+        btn_cancel.setOnClickListener(this);
+        mViewProvince.addChangingListener(this);
+        mViewCity.addChangingListener(this);
+        mViewDistrict.addChangingListener(this);
+
+        Toast.makeText(activity,mProvinceDatas.length+"",Toast.LENGTH_SHORT).show();
+
+        mViewProvince.setViewAdapter(new ArrayWheelAdapter<String>(activity, mProvinceDatas));
+        // 设置可见条目数量
+        mViewProvince.setVisibleItems(7);
+        mViewCity.setVisibleItems(7);
+        mViewDistrict.setVisibleItems(7);
+        updateCities();
+        updateAreas();
+
+        //设置PopupWindow的View
+        this.setContentView(mView);
+        //设置PopupWindow弹出窗体的宽
+        this.setWidth(LinearLayout.LayoutParams.MATCH_PARENT);
+        //设置PopupWindow弹出窗体的高
+        this.setHeight(LinearLayout.LayoutParams.WRAP_CONTENT);
+        //设置PopupWindow弹出窗体可点击
+        this.setFocusable(true);
+        //设置SelectPicPopupWindow弹出窗体动画效果
+        this.setAnimationStyle(R.style.Animation);
+        this.setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        this.setOutsideTouchable(true);
+
+        this.setOnDismissListener(new OnDismissListener(){
+            @Override
+            public void onDismiss() {
+                close();
+            }
+        });
+    }
+
+    public void show(){
+        showAtLocation(activity.getWindow().getDecorView(), Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL, 0, 0);
+        backgroundAlpha(0.6f);
+    }
+
+    public void close(){
+        releaseProvinceData();
+        backgroundAlpha(1f);
+        this.dismiss();
+    }
+
+    // 设置屏幕透明度
+    public void backgroundAlpha(float bgAlpha) {
+        WindowManager.LayoutParams lp = activity.getWindow().getAttributes();
+        lp.alpha = bgAlpha; // 0.0~1.0
+        activity.getWindow().setAttributes(lp);
+    }
+
+    public void setListener(WheelPickerComfirmListener wheelPickerComfirmListener){
+        this.listener = wheelPickerComfirmListener;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.btn_cancel:
+                close();
+                break;
+
+            case R.id.btn_confirm:
+                if(listener != null){
+                    listener.onSelected(mCurrentProviceName,mCurrentCityName,mCurrentDistrictName,mCurrentZipCode);
+                }
+                close();
+                break;
+        }
+    }
+
+    @Override
+    public void onChanged(WheelView wheel, int oldValue, int newValue) {
+        if (wheel == mViewProvince) {
+            updateCities();
+        } else if (wheel == mViewCity) {
+            updateAreas();
+        } else if (wheel == mViewDistrict) {
+            mCurrentDistrictName = mDistrictDatasMap.get(mCurrentCityName)[newValue];
+            mCurrentZipCode = mZipcodeDatasMap.get(mCurrentDistrictName);
+        }
+    }
+
+    /**
+     * 根据当前的市，更新区WheelView的信息
+     */
+    private void updateAreas() {
+        int pCurrent = mViewCity.getCurrentItem();
+        mCurrentCityName = mCitisDatasMap.get(mCurrentProviceName)[pCurrent];
+        String[] areas = mDistrictDatasMap.get(mCurrentCityName);
+
+        if (areas == null) {
+            areas = new String[] { "" };
+        }
+        mViewDistrict.setViewAdapter(new ArrayWheelAdapter<String>(activity, areas));
+        mViewDistrict.setCurrentItem(0);
+    }
+
+    /**
+     * 根据当前的省，更新市WheelView的信息
+     */
+    private void updateCities() {
+        int pCurrent = mViewProvince.getCurrentItem();
+        mCurrentProviceName = mProvinceDatas[pCurrent];
+        String[] cities = mCitisDatasMap.get(mCurrentProviceName);
+        if (cities == null) {
+            cities = new String[] { "" };
+        }
+        mViewCity.setViewAdapter(new ArrayWheelAdapter<String>(activity, cities));
+        mViewCity.setCurrentItem(0);
+        updateAreas();
+    }
+
+    public interface WheelPickerComfirmListener{
+        public void onSelected(String Province,String City,String District,String PostCode);
+    }
+}
